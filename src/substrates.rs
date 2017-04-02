a87dff8e5564321e12551b97a6418c56c84f0698
Petter Johansson <pettjoha@kth.se>
2017-04-02 23:03:57 +0200

    Add residue constructors to ResidueBase
    This improves readability by a large margin.

--- src/substrates.rs
+++ src/substrates.rs
@@ -27,15 +27,48 @@ pub enum SubstrateType {
 }
 use self::SubstrateType::*;
 
-// This is a base for generating atoms belonging to a residue.
-// Every residue has a name and a list of atoms that belong to it
-// with their base coordinates. The names are static since these
-// should all be generated only once from a single source.
+/// This is a base for generating atoms belonging to a residue.
+/// Every residue has a name and a list of atoms that belong to it
+/// with their base coordinates. The names are static since these
+/// should all be generated only once from a single source.
 struct ResidueBase {
     code: &'static str,
     atoms: Vec<ResidueAtom>
 }
-#[derive(Clone)]
+
+impl ResidueBase {
+    /// Graphene is a single carbon atom at each lattice point.
+    fn graphene(bond_length: f64) -> ResidueBase {
+        ResidueBase {
+            code: "GRPH",
+            atoms: vec![
+                ResidueAtom {
+                    code: "C",
+                    position: Coord::new(bond_length/2.0, bond_length/2.0, bond_length/2.0)
+                }
+            ]
+        }
+    }
+
+    /// Silica is a rigid SiO2 molecule at each lattice point.
+    fn silica(bond_length: f64) -> ResidueBase {
+        let z0 = 0.000;
+        let dz = 0.151;
+        let base_coord = Coord::new(bond_length/4.0, bond_length/6.0, z0);
+
+        ResidueBase {
+            code: "SIO",
+            atoms: vec![
+                ResidueAtom { code: "O1", position: base_coord.add_manual(0.0, 0.0,  dz)},
+                ResidueAtom { code: "SI", position: base_coord },
+                ResidueAtom { code: "O2", position: base_coord.add_manual(0.0, 0.0, -dz)}
+            ]
+        }
+    }
+}
+
+/// Every atom in a residue has their own code and relative position
+#[derive(Clone, Copy)]
 struct ResidueAtom  {
     code: &'static str,
     position: Coord
@@ -56,17 +89,17 @@ pub fn create_substrate((size_x, size_y): (f64, f64), substrate_type: SubstrateT
     Ok(substrate)
 }
 
-// Create a graphene layer of desired input size.
-//
-// The layer consists of a hexagonal grid of carbon atoms
-// which is created with a bond length of 0.142 nm. To ensure
-// that the system can be periodically replicated along x and y
-// the dimensions are trimmed to the closest possible size
-// that fits an even number of replicas.
+/// Create a graphene layer of desired size.
+///
+/// The layer consists of a hexagonal lattice of carbon atoms
+/// which is created with a bond length of 0.142 nm. To ensure
+/// that the system can be periodically replicated along x and y
+/// the dimensions are trimmed to the closest possible size
+/// that fits an even number of replicas.
 fn create_graphene(size_x: f64, size_y: f64) -> AtomSystem {
     let bond_length = 0.142;
     let z0 = bond_length;
-    let residue_base = get_graphene_base(bond_length);
+    let residue_base = ResidueBase::graphene(bond_length);
 
     let crystal = Crystal::from_type(Hexagonal { length: bond_length });
     let lattice = Lattice::from_size(&crystal, size_x, size_y)
@@ -79,10 +112,15 @@ fn create_graphene(size_x: f64, size_y: f64) -> AtomSystem {
     }
 }
 
+/// Create a silica layer of desired size.
+///
+/// The layer consists of a triclinic lattice where the spacing
+/// is 0.45 along both vectors and the angle between them
+/// is 60 degrees. At each lattice point an SiO2 molecule is placed.
 fn create_silica(size_x: f64, size_y: f64) -> AtomSystem {
     let bond_length = 0.450;
     let z0 = 0.30;
-    let residue_base = get_silica_base(bond_length);
+    let residue_base = ResidueBase::silica(bond_length);
 
     let angle = 60.0f64.to_radians();
     let crystal = Crystal::from_type(Triclinic { a: bond_length, b: bond_length, gamma: angle });
@@ -99,54 +137,21 @@ fn create_silica(size_x: f64, size_y: f64) -> AtomSystem {
 // Use a constructed grid and generate atoms of a residue for them
 fn gen_atom_list(coords: &Vec<Coord>, residue: ResidueBase) -> Vec<Atom> {
     let mut atoms: Vec<Atom> = Vec::new();
-    for (i, point) in coords.iter().enumerate() {
-        for (j, atom) in residue.atoms.iter().enumerate() {
-            atoms.push(get_atom(i, j, point, atom, &residue));
+
+    for (residue_number, lattice_point) in coords.iter().enumerate() {
+        for (atom_number, residue_atom) in residue.atoms.iter().enumerate() {
+            let atom = Atom {
+                residue_name: residue.code.to_string(),
+                residue_number: residue_number as u64,
+                atom_name: residue_atom.code.to_string(),
+                atom_number: (residue.atoms.len()*residue_number) as u64 + (atom_number as u64),
+                position: lattice_point.add(&residue_atom.position)
+            };
+            atoms.push(atom);
         }
     }
 
     atoms
-}
-
-fn get_atom(residue_number: usize, atom_number: usize, grid_point: &Coord,
-            atom: &ResidueAtom, residue: &ResidueBase) -> Atom {
-    Atom {
-        residue_name: residue.code.to_string(),
-        residue_number: residue_number as u64,
-        atom_name: atom.code.to_string(),
-        atom_number: (residue.atoms.len()*residue_number) as u64 + (atom_number as u64),
-        position: grid_point.add(&atom.position)
-    }
-}
-
-// A base graphene molecule is only a carbon atom.
-fn get_graphene_base(bond_length: f64) -> ResidueBase {
-    ResidueBase {
-        code: "GRPH",
-        atoms: vec![
-            ResidueAtom {
-                code: "C",
-                position: Coord::new(bond_length/2.0, bond_length/2.0, bond_length/2.0)
-            }
-        ]
-    }
-}
-
-// A base silica molecule consists of three atoms.
-fn get_silica_base(bond_length: f64) -> ResidueBase {
-    let z0 = 0.000;
-    let dz = 0.151;
-
-    let base_coord = Coord::new(bond_length/4.0, bond_length/6.0, z0);
-
-    ResidueBase {
-        code: "SIO",
-        atoms: vec![
-            ResidueAtom { code: "O1", position: base_coord.add_manual(0.0, 0.0,  dz)},
-            ResidueAtom { code: "SI", position: base_coord },
-            ResidueAtom { code: "O2", position: base_coord.add_manual(0.0, 0.0, -dz)}
-        ]
-    }
 }
 
 #[cfg(test)]
