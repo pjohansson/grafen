568826c060a0be5ed884a29ab08afa01035f0222
Petter Johansson <pettjoha@kth.se>
2017-05-31 14:46:59 +0200

    Fix atom numbering output in `write_gromos`

--- src/bin/output.rs
+++ src/bin/output.rs
@@ -5,7 +5,7 @@ use grafen::system::System;
 
 use std::fs::File;
 use std::io::{BufWriter, Write};
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 /// Output a system to disk as a GROMOS formatted file.
 /// The filename extension is adjusted to .gro.
@@ -13,20 +13,24 @@ use std::path::{Path, PathBuf};
 /// # Errors
 /// Returns an error if the file could not be written to.
 pub fn write_gromos(system: &System, output_file: &Path, title: &str) -> Result<()> {
-    let path = PathBuf::from(output_file).with_extension("gro");
-    let file = File::create(&path)?;
+    let path = output_file.with_extension("gro");
+    let file = File::create(path)?;
     let mut writer = BufWriter::new(file);
 
     writer.write_fmt(format_args!("{}\n", title))?;
     writer.write_fmt(format_args!("{}\n", system.num_atoms()))?;
 
+    // Absolute atom numbering.
+    let mut j = 0;
+
     for (i, residue) in system.residues.iter().enumerate() {
-        for (j, atom) in residue.base.atoms.iter().enumerate() {
+        for atom in residue.base.atoms.iter() {
             // GROMOS files wrap atom and residue numbering after five digits
             // so we must output at most that. We also switch to indexing the
             // numbers from 1 instead of from 0.
             let residue_number = (i + 1) % 100_000;
             let atom_number = (j + 1) % 100_000;
+            j += 1;
 
             let position = residue.position + atom.position;
             let (x, y, z) = position.to_tuple();
