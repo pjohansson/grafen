3000c18c0509365d5551c52254a9e9e35f391778
Petter Johansson <pettjoha@kth.se>
2019-05-10 14:00:46 +0200

    Replace deprecated `home_dir` with `dirs` crate

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -15,8 +15,9 @@ use grafen::{
 };
 
 use colored::*;
+use dirs;
 use std::{
-    env::{current_dir, home_dir, var, var_os},
+    env::current_dir,
     fs::DirBuilder,
     path::PathBuf,
     process,
@@ -131,94 +132,54 @@ fn read_input_configurations(confs: Vec<PathBuf>) -> (Vec<ComponentEntry>, Vec<C
 }
 
 fn read_or_create_default_database() -> Result<DataBase> {
-    let default_database_paths = get_default_database_paths();
+    let path = match get_default_database_path() {
+        Some(path) => path,
+        None => {
+            eprintln!(
+                "{}",
+                format!(
+                    "Could not find a location for the default database. \
+                    Opening a temporary database which cannot be saved.",
+                )
+                .color("yellow")
+            );
 
-    if default_database_paths.is_empty() {
-        eprintln!(
-            "{}",
-            format!(
-                "Could not find a location for the default database. \
-                 Opening a database which cannot be saved.",
-            )
-            .color("yellow")
-        );
-
-        return Ok(DataBase::new());
-    }
-
-    // See if a default database can be found at any path before creating a new one.
-    for path in &default_database_paths {
-        if path.is_file() {
-            return read_database(&path).map_err(|err| GrafenCliError::from(err));
-        }
-    }
-
-    let mut default_database = DataBase::new();
-    let default_path = &default_database_paths[0];
-
-    if let Some(parent_dir) = default_path.parent() {
-        match DirBuilder::new().recursive(true).create(&parent_dir) {
-            Ok(_) => default_database.set_path(&default_path).unwrap(),
-            Err(err) => {
-                eprintln!(
-                    "{}",
-                    format!(
-                        "Warning: Could not create a folder for a default database at '{}' ({}). \
-                         Opening a database which cannot be saved.",
-                        default_path.display(),
-                        err
-                    )
-                    .color("yellow")
-                );
-            }
-        }
-    }
-
-    Ok(default_database)
-}
-
-fn get_default_database_paths() -> Vec<PathBuf> {
-    get_platform_dependent_data_dirs()
-        .into_iter()
-        .map(|dir| dir.join("grafen").join(DEFAULT_DBNAME))
-        .collect()
-}
-
-fn get_platform_dependent_data_dirs() -> Vec<PathBuf> {
-    let xdg_data_dirs_variable =
-        var("XDG_DATA_DIRS").unwrap_or(String::from("/usr/local/share:/usr/local"));
-    let xdg_dirs_iter = xdg_data_dirs_variable
-        .split(':')
-        .map(|s| Some(PathBuf::from(s)));
-
-    let dirs = if cfg!(target_os = "macos") {
-        vec![
-            var_os("XDG_DATA_HOME").map(|dir| PathBuf::from(dir)),
-            home_dir().map(|dir| dir.join("Library").join("Application Support")),
-        ]
-        .into_iter()
-        .chain(xdg_dirs_iter)
-        .chain(vec![Some(
-            PathBuf::from("/")
-                .join("Library")
-                .join("Application Support"),
-        )])
-        .collect()
-    } else if cfg!(target_os = "linux") {
-        vec![
-            var_os("XDG_DATA_HOME").map(|dir| PathBuf::from(dir)),
-            home_dir().map(|dir| dir.join(".local").join("share")),
-        ]
-        .into_iter()
-        .chain(xdg_dirs_iter)
-        .collect()
-    } else if cfg!(target_os = "windows") {
-        vec![var_os("APPDATA").map(|dir| PathBuf::from(dir))]
-    } else {
-        Vec::new()
+            return Ok(DataBase::new());
+        },
     };
 
-    dirs.into_iter().filter_map(|dir| dir).collect()
+    // See if a database can be found before creating a new one.
+    if path.is_file() {
+        read_database(&path).map_err(|err| GrafenCliError::from(err))
+    } else {
+        let mut default_database = DataBase::new();
+        let default_path = path;
+
+        // Try to create the database directory and update the path
+        if let Some(parent_dir) = default_path.parent() {
+            match DirBuilder::new().recursive(true).create(&parent_dir) {
+                Ok(_) => default_database.set_path(&default_path).unwrap(),
+                Err(err) => {
+                    eprintln!(
+                        "{}",
+                        format!(
+                            "Warning: Could not create a folder for a default database at '{}' ({}). \
+                            Opening a database which cannot be saved.",
+                            default_path.display(),
+                            err
+                        )
+                        .color("yellow")
+                    );
+                }
+            }
+        }
+
+        Ok(default_database)
+    }
+}
+
+fn get_default_database_path() -> Option<PathBuf> {
+    dirs::data_local_dir().map(|dir| dir.join("grafen").join(DEFAULT_DBNAME))
 }
 
 #[cfg(test)]
@@ -226,73 +187,17 @@ pub mod tests {
     use super::*;
     use std::env::set_var;
     use std::path::Component;
-
-    #[test]
-    #[cfg(target_os = "macos")]
-    fn default_database_dirs_on_macos_lead_with_xdg_dirs_then_application_support() {
-        let xdg_data_home = "data_home";
-        set_var("XDG_DATA_HOME", xdg_data_home);
-
-        let xdg_data_directories = vec!["data_dir1", "data_dir2"];
-        let xdg_data_dirs = format!("{}:{}", xdg_data_directories[0], xdg_data_directories[1]);
-        set_var("XDG_DATA_DIRS", xdg_data_dirs);
-
-        let user_appsupport = home_dir()
-            .unwrap()
-            .join("Library")
-            .join("Application Support");
-        let root_appsupport = PathBuf::from("/")
-            .join("Library")
-            .join("Application Support");
-
-        let result = get_platform_dependent_data_dirs();
-        let priority_list = vec![
-            PathBuf::from(xdg_data_home),
-            user_appsupport,
-            PathBuf::from(xdg_data_directories[0]),
-            PathBuf::from(xdg_data_directories[1]),
-            root_appsupport,
-        ];
-
-        assert_eq!(result, priority_list);
-    }
-
-    #[test]
-    #[cfg(target_os = "linux")]
-    fn default_database_dirs_on_linux_lead_with_xdg_dirs_then_local_share() {
-        let xdg_data_home = "data_home";
-        set_var("XDG_DATA_HOME", xdg_data_home);
-
-        let xdg_data_directories = vec!["data_dir1", "data_dir2"];
-        let xdg_data_dirs = format!("{}:{}", xdg_data_directories[0], xdg_data_directories[1]);
-        set_var("XDG_DATA_DIRS", xdg_data_dirs);
-
-        let user_local_share = home_dir().unwrap().join(".local").join("share");
-
-        let result = get_platform_dependent_data_dirs();
-        let priority_list = vec![
-            PathBuf::from(xdg_data_home),
-            user_local_share,
-            PathBuf::from(xdg_data_directories[0]),
-            PathBuf::from(xdg_data_directories[1]),
-        ];
-
-        assert_eq!(result, priority_list);
-    }
-
     #[test]
     #[cfg(any(unix, windows))]
     fn default_database_path_adds_grafen_directory_and_database_path() {
-        let dirs = get_default_database_paths();
-        assert!(!dirs.is_empty());
+        let path = get_default_database_path().unwrap();
+        let mut iter = path.components().rev();
 
-        for path in dirs {
-            let mut iter = path.components().rev();
-            assert_eq!(
-                iter.next().unwrap(),
-                Component::Normal(DEFAULT_DBNAME.as_ref())
-            );
-            assert_eq!(iter.next().unwrap(), Component::Normal("grafen".as_ref()));
-        }
+        assert_eq!(
+            iter.next().unwrap(),
+            Component::Normal(DEFAULT_DBNAME.as_ref())
+        );
+
+        assert_eq!(iter.next().unwrap(), Component::Normal("grafen".as_ref()));
     }
 }
